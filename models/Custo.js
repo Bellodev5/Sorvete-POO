93c053ba0964992803b67424820a20ef7f18d8c7
GUILHERME BELLO <guilherme_bello@estudante.sesisenai.org.br>
2026-03-26 07:47:52 -0300

    correção da classe custo.js e criação index.js

--- models/Custo.js
+++ models/Custo.js
@@ -2,13 +2,13 @@ export default class Custo {
     constructor(
         leite  = 4.50,
         acucar = 4.89,
-        gema   = 0.80,
+        creme = 12.90,
         frutasV = 35.00,
         acidoC  = 18.00
     ) {
         this.leite   = leite
         this.acucar  = acucar
-        this.gema    = gema
+        this.creme   = creme
         this.frutasV = frutasV
         this.acidoC  = acidoC
 
@@ -22,7 +22,7 @@ export default class Custo {
         this.precoIngredientes = {
             leite:   Number(((qtdeIngredientes.leite   / 1000) * this.leite).toFixed(2)),
             acucar:  Number(((qtdeIngredientes.acucar  / 1000) * this.acucar).toFixed(2)),
-            gema:    Number((qtdeIngredientes.gema * this.gema).toFixed(2)),
+            creme:   Number(((qtdeIngredientes.creme   / 1000) * this.creme).toFixed(2)),
             frutasV: Number(((qtdeIngredientes.frutasV / 1000) * this.frutasV).toFixed(2)),
             acidoC:  Number(((qtdeIngredientes.acidoC  / 1000) * this.acidoC).toFixed(2)), 
         }
@@ -30,7 +30,7 @@ export default class Custo {
         const soma =
             this.precoIngredientes.leite   +
             this.precoIngredientes.acucar  +
-            this.precoIngredientes.gema    +
+            this.precoIngredientes.creme   +
             this.precoIngredientes.frutasV +
             this.precoIngredientes.acidoC
 
